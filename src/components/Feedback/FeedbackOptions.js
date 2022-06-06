30dd2a0d8e93f3c84f48775fe3d3401ac1b4748f
OleksandrLakhno <baikerstreet221bua@gmail.com>
2022-06-06 21:31:25 +0300

    add :active

--- src/components/Feedback/FeedbackOptions.js
+++ src/components/Feedback/FeedbackOptions.js
@@ -2,17 +2,17 @@ import React from "react";
 import PropTypes from 'prop-types';
 import f from './Feedback.module.css';
 
-function FeedbackOptions({ onIncrement,options }) {
+function FeedbackOptions({ onIncrement, options }) {
   return (
           <div>
       {options.map(option => (
-        <button className={f.buttons} name={ option} key={option} onClick={onIncrement}>
+        <button className={f.buttons} name={option} key={option} onClick = { onIncrement }>
           { option.toUpperCase()}
         </button>
       ))}
           </div>
   )
-}
+};
 
 FeedbackOptions.propTypes = {
   options: PropTypes.arrayOf(PropTypes.string).isRequired,
